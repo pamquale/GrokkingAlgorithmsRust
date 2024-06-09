3ecbad0064828d6952bd7469dd8db81b0727075d
pamquale <pamquale@gmail.com>
2024-06-09 22:01:08 +0300

    added selection sort

--- selection_sort/src/main.rs
+++ selection_sort/src/main.rs
@@ -1,3 +1,28 @@
+fn selection_sort(arr: &mut [i32]) {
+    let n = arr.len();
+
+    for i in 0..n {
+        let mut min_index = i;
+
+        for j in (i + 1)..n {
+            if arr[j] < arr[min_index] {
+                min_index = j;
+            }
+        }
+
+        if min_index != i {
+            arr.swap(i, min_index);
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut arr = [4, 512, 64, 1, 256, 32, 2048, 128, 2, 1024, 8, 16];
+
+    println!("Unsorted array: {:?}", arr);
+
+    selection_sort(&mut arr);
+
+    println!("Sorted array: {:?}", arr);
+
 }
