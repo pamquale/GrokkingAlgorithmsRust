da0c4e07444ce46ab65d6eb89bfa52057d381c86
pamquale <86326514+pamquale@users.noreply.github.com>
2024-06-09 14:06:16 +0300

    Added some debug to the binary search

--- binary_search/src/main.rs
+++ binary_search/src/main.rs
@@ -1,16 +1,17 @@
 fn binary_search(arr: &[i32], target: i32) -> Option<usize> {
     let mut low = 0;
-    let mut high = arr.len() - 1;
+    let mut top = arr.len() - 1;
 
-    while low <= high {
-        let mid = (low + high) / 2;
+    while low <= top {
+        let mid = (low + top) / 2;
         let guess = arr[mid];
+        println!("low: {} mid: {} top: {} guess: {}", low, mid, top, guess);
 
         if guess == target {
             return Some(mid);
         } else if guess > target {
             if mid == 0 { break; }
-            high = mid - 1;
+            top = mid - 1;
         } else {
             low = mid + 1;
         }
@@ -20,11 +21,11 @@ fn binary_search(arr: &[i32], target: i32) -> Option<usize> {
 }
 
 fn main() {
-    let arr = [1, 2, 3, 5, 7, 9, 12, 32, 43, 55, 67, 98];
-    let target = 67;
+    let arr = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048];
+    let target = 512;
 
     match binary_search(&arr, target) {
-        Some(index) => println!("Found at index: {}", index),
-        None => println!("Not found"),
+        Some(index) => println!("Found at index {}!", index),
+        None => println!("Item was not found."),
     }
 }
