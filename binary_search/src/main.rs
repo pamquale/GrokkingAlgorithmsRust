32e1be03d3fc602a8ae56c5449ca2fef4c6b9ec1
pamquale <pamquale@gmail.com>
2024-06-09 21:36:16 +0300

    Resolve merge conflicts

--- binary_search/src/main.rs
+++ binary_search/src/main.rs
@@ -1,3 +1,33 @@
+fn binary_search(arr: &[i32], target: i32) -> Option<usize> {
+    let mut low = 0;
+    let mut top = arr.len() - 1;
+
+    while low <= top {
+        let mid = (low + top) / 2;
+        let guess = arr[mid];
+        let low_num = arr[low];
+        let top_num = arr[top];
+        println!("low: [{}-{}] mid: [{}-{}] top: [{}-{}] \nguess: {}", low, low_num, mid, guess, top, top_num, guess);
+
+        if guess == target {
+            return Some(mid);
+        } else if guess > target {
+            if mid == 0 { break; }
+            top = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    None
+}
+
 fn main() {
-    println!("Hello, world!");
+    let arr = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048];
+    let target = 512;
+
+    match binary_search(&arr, target) {
+        Some(index) => println!("Found {} at index {}!", target, index),
+        None => println!("Item {} was not found.", target),
+    }
 }
